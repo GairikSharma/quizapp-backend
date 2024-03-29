c45a890a43b83f011246c35f9d0b36b3ccf403e1
GairikSharma <gairiksharma202@gmail.com>
2024-03-30 01:42:49 +0530

    Changes

--- controllers/dsa/dsaApi.js
+++ controllers/dsa/dsaApi.js
@@ -3,11 +3,10 @@ const { dsaQuestion } = require("../../models/quizScheema");
 //creating new dsa question
 const createDSAQuestion = async (req, res) => {
   //get the sent in data off req body
-  const {question, options, correctOption, point} = req.body
-  // const question = req.body.question;
-  // const options = req.body.options;
-  // const correctOption = req.body.correctOption;
-  // const point = req.body.point;
+  const question = req.body.question;
+  const options = req.body.options;
+  const correctOption = req.body.correctOption;
+  const point = req.body.point;
 
   //create a question with it
   const qs = await dsaQuestion.create({
--- server.js
+++ server.js
@@ -26,30 +26,30 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// app.get(
-//   "/all-questions-quantitative-aptitude",
-//   quantQuestionController.all_quant_qs
-// );
-// app.get("/all-questions-verbal", verbalQuestionController.all_verbal_qs);
-// app.get("/all-questions-computer-networks", cnQuestionController.all_cn_qs);
-// app.get("/all-questions-dbms", dbmsQuestionController.all_dbms_qs);
-// app.get("/all-questions-dsa", dsaQuestionController.all_dsa_qs);
-// app.get("/all-questions-oop", oopQuestionController.all_oop_qs);
+app.get(
+  "/all-questions-quantitative-aptitude",
+  quantQuestionController.all_quant_qs
+);
+app.get("/all-questions-verbal", verbalQuestionController.all_verbal_qs);
+app.get("/all-questions-computer-networks", cnQuestionController.all_cn_qs);
+app.get("/all-questions-dbms", dbmsQuestionController.all_dbms_qs);
+app.get("/all-questions-dsa", dsaQuestionController.all_dsa_qs);
+app.get("/all-questions-oop", oopQuestionController.all_oop_qs);
 
 // "/all-questions?topic=oop"
 // "all-questions/:topic"
 
 // app.post("/new-question/:topic", questionControllers.newQuestion);
 
-// app.post(
-//   "/new-question/quantitative-aptitude",
-//   quantQuestionController.new_quant_qs
-// );
-// app.post("/new-question/verbal", verbalQuestionController.new_verbal_qs);
-// app.post("/new-question/oop", oopQuestionController.new_oop_qs);
-// app.post("/new-question/dsa", dsaQuestionController.new_dsa_qs);
-// app.post("/new-question/dbms", dbmsQuestionController.new_dbms_qs);
-// app.post("/new-question/computer-networks", cnQuestionController.new_cn_qs);
+app.post(
+  "/new-question/quantitative-aptitude",
+  quantQuestionController.new_quant_qs
+);
+app.post("/new-question/verbal", verbalQuestionController.new_verbal_qs);
+app.post("/new-question/oop", oopQuestionController.new_oop_qs);
+app.post("/new-question/dsa", dsaQuestionController.new_dsa_qs);
+app.post("/new-question/dbms", dbmsQuestionController.new_dbms_qs);
+app.post("/new-question/computer-networks", cnQuestionController.new_cn_qs);
 
 app.listen(process.env.PORT, () => {
   console.log(`server running at PORT ${process.env.PORT}`);
